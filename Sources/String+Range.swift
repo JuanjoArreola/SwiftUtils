eae12731760bc81c845075fb4b62f2a9ecafdbdf
Juanjo <juanjo.arreola@gmail.com>
2017-03-15 16:49:13 -0600

    Fix string range utf

--- Sources/String+Range.swift
+++ Sources/String+Range.swift
@@ -9,14 +9,29 @@
 import Foundation
 
 public extension String {
-    func range(from range: NSRange) -> Range<String.Index>? {
-        guard let start = index(startIndex, offsetBy: range.location, limitedBy: endIndex),
-            let end = index(start, offsetBy: range.length, limitedBy: endIndex) else { return nil }
-        return start..<end
+    
+    func range(from nsRange: NSRange) -> Range<String.Index>? {
+        guard let from16 = utf16.index(utf16.startIndex, offsetBy: nsRange.location, limitedBy: utf16.endIndex),
+            let to16 = utf16.index(from16, offsetBy: nsRange.length, limitedBy: utf16.endIndex),
+            let from = from16.samePosition(in: self),
+            let to = to16.samePosition(in: self)
+            else { return nil }
+        return from..<to
     }
     
     func nsRange(from range: Range<String.Index>) -> NSRange {
-        return NSRange.init(location: distance(from: startIndex, to: range.lowerBound),
-                            length: distance(from: range.lowerBound, to: range.upperBound))
+        let from = range.lowerBound.samePosition(in: utf16)
+        let to = range.upperBound.samePosition(in: utf16)
+        let location = utf16.distance(from: utf16.startIndex, to: from)
+        let length = utf16.distance(from: from, to: to)
+        return NSRange(location: location, length: length)
+    }
+    
+    var wholeRange: Range<String.Index> {
+        return startIndex..<endIndex
+    }
+    
+    var wholeNSRange: NSRange {
+        return NSRange(location: 0, length: characters.count)
     }
 }

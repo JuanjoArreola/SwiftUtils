c062c955be780d32215dd0ff3fd101be6caf7483
Juanjo <juanjo.arreola@gmail.com>
2017-08-16 12:18:21 -0500

    public UIView extension

--- SwiftUtils.podspec
+++ SwiftUtils.podspec
@@ -1,7 +1,7 @@
 Pod::Spec.new do |s|
 
   s.name         = "SwiftUtils"
-  s.version      = "1.2.0"
+  s.version      = "1.2.1"
   s.summary      = "A collection of utilities for iOS projects"
 
   s.homepage     = "https://github.com/JuanjoArreola/SwiftUtils"
--- SwiftUtils/UIView+Util.swift
+++ SwiftUtils/UIView+Util.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-extension UIView {
+public extension UIView {
     func setCornerRadius(_ radius: CGFloat, borderColor: UIColor? = nil, borderWidth: CGFloat = 1.0) {
         self.layer.cornerRadius = radius
         self.layer.masksToBounds = true
